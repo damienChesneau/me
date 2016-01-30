022ad0c6e7969466dd4259d16da064b47f683b01
Damien Chesneau <ches.damien@gmail.com>
2016-01-30 16:16:23 +0100

    Revert "Revert "New email.""
    This reverts commit e1fa356100403bbe2e1f852e693ea26d19d9d231.
    
    Change-Id: I13ca9920f4251206f6a0e4e3c5e29a8263f97d98

--- content.php
+++ content.php
@@ -13,7 +13,7 @@
     <a class="email" href="mailto:ches.damien@gmail.com">
         <span class="icon"></span>
         <span class="iconHover"></span>
-        ches.damien@gmail.com
+       contact@damienchesneau.fr 
     </a>
     <ul>
         <li>
